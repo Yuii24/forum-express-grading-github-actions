06fdd9f6146109f9f8c2bcc58513d91b61aef0bd
Yuii24 <linian2040@gmail.com>
2024-08-05 15:53:03 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
+const path = require('path')
 const express = require('express')
+const handlebar = require('express-handlebars')
+const session = require('express-session')
+const passport = require('./config/passport')
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./helpers/auth-helpers')
+
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
+// const db = require('./models')
+
+app.engine('hbs', handlebar({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
