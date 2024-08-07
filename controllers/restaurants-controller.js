6f1fe5daa91de6fe426ffd2c533b230f7f1a0b55
Yuii24 <linian2040@gmail.com>
2024-08-07 14:28:35 +0800

    delete unnecessary code

--- controllers/restaurants-controller.js
+++ controllers/restaurants-controller.js
@@ -15,36 +15,6 @@ const restaurantController = {
         restaurants: data
       })
     })
-  },
-  getRestaurant: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, {
-      include: Category // 拿出關聯的 Category model
-    })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-
-        return restaurant.increment('viewCounts', { by: 1 })
-
-          .then(restaurant => res.render('restaurant', {
-            restaurant: restaurant.toJSON()
-          }))
-      })
-      .catch(err => next(err))
-  },
-  getDashboard: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, {
-      raw: true,
-      nest: true,
-      include: Category
-    })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-
-        console.log(restaurant)
-        return res.render('dashboard', { restaurant })
-      })
-
-      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
