92b67984861ab0699ac7c893acefc14359bf7c6c
Yuii24 <linian2040@gmail.com>
2024-08-15 01:41:50 +0800

    api completed

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -3,17 +3,18 @@ const router = express.Router()
 const passport = require('../../config/passport')
 const admin = require('./modules/admin')
 
-// const { authenticated } = require('../../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../../middleware/apiAuth')
+
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
 const restController = require('../../controllers/apis/restaurant-controller')
 const userController = require('../../controllers/apis/user-controller')
 
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
+
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
-
-router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', apiErrorHandler)
 
