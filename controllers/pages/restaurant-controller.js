8141575a118c78ab70aa142962cf3aeda6027580
Yuii24 <linian2040@gmail.com>
2024-08-14 11:52:38 +0800

    extract services code

--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,50 +1,10 @@
 const { User, Comment, Restaurant, Category } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+// const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+const restaurantsServices = require('../../services/restaurant-services')
 
 const resaturantsController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9
-
-    const categoryId = Number(req.query.categoryId) || ''
-
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-
-    // 下方的展開運算值內容等與這兩行:
-    // 『 const where = {}
-    //    if (categoryId) where.categoryId = categoryId  』
-
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        include: Category,
-        where: {
-          ...categoryId ? { categoryId } : {}
-        },
-        limit,
-        offset,
-        nest: true,
-        raw: true
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const FavoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
-        const LikedRestaurantId = req.user && req.user.LikedRestaurants.map(lr => lr.id)
-        const data = restaurants.rows.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50),
-          isFavorited: FavoritedRestaurantsId.includes(r.id),
-          isLiked: LikedRestaurantId.includes(r.id)
-        }))
-        return res.render('restaurants', {
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
-      .catch(err => next(err))
+    restaurantsServices.getRestaurants(req, res, (err, data) => err ? next(err) : res.render('restaurants', data))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
